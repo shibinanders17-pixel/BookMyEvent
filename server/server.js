43e528ff78d97b83a7890c797684ec339907db69
Shibin <shibinanders17@gmail.com>
2026-05-18 22:51:39 +0530

    baseURL changes

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const startServer = async () => {
   }
 
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 };
 
 startServer();

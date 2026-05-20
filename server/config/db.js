4de11a2a4534c801dd6c3836e56a3d4eef204c69
Shibin <shibinanders17@gmail.com>
2026-05-20 11:53:28 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      family : 4,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
